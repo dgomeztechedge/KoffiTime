a5195929878a3cf97fba228e8353e4909faae39d
dgomeztechedge <daniel.gomez@techedgegroup.com>
2019-12-16 13:44:04 +0100

    Resizable font

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,21 @@
 import { Component, AfterViewInit } from '@angular/core';
 import { ViewChild, ElementRef } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements AfterViewInit {
-  @ViewChild("canvas", { static: false }) Canvas: ElementRef;
+  @ViewChild('canvas', { static: false }) Canvas: ElementRef;
   public context: CanvasRenderingContext2D;
-  title = "KoffiTime";
-  texto = "KoffiTime";
-  rutaImagen = "../assets/imgs/coffe.png";
+  title = 'KoffiTime';
+  texto = 'KoffiTime';
+  rutaImagen = '../assets/imgs/coffe.png';
   fuente = 48;
   ngAfterViewInit(): void {
     this.context = (this.Canvas.nativeElement as HTMLCanvasElement).getContext(
-      "2d"
+      '2d'
     );
     if (this.context) {
       this.drawImage();
@@ -27,9 +26,9 @@ export class AppComponent implements AfterViewInit {
     const arrayFrase = this.contarTexto(`"${this.texto}"`);
     let y = 250;
     setTimeout(() => {
-      this.context.font = "48px Palatino";
-      this.context.fillStyle = "#FFFFFF";
-      this.context.textAlign = "center";
+      this.context.font = `${this.fuente}px Palatino`;
+      this.context.fillStyle = '#FFFFFF';
+      this.context.textAlign = 'center';
       if (arrayFrase.length !== 1) {
         y = y - (this.fuente + 10) * (arrayFrase.length / 2);
       }
@@ -42,14 +41,16 @@ export class AppComponent implements AfterViewInit {
     }, 250);
   }
   contarTexto(text: string): string[] {
-    const arrayText = text.split(" ");
+    const arrayText = text.split(' ');
     const arrayFinal: string[] = [];
-    let stringTemporal = "";
+    let stringTemporal = '';
+    const fraseSize = (1152 / this.fuente);
     arrayText.forEach((x, index) => {
       stringTemporal = `${stringTemporal}${x} `;
-      if (stringTemporal.length >= 23 || index === arrayText.length - 1) {
+      if (stringTemporal.length >= fraseSize || index === arrayText.length - 1
+      ) {
         arrayFinal.push(stringTemporal);
-        stringTemporal = "";
+        stringTemporal = '';
       }
     });
     return arrayFinal;
@@ -59,6 +60,6 @@ export class AppComponent implements AfterViewInit {
     img.onload = x => {
       this.context.drawImage(img, 0, 0);
     };
-    img.src = "../assets/imgs/coffe.png";
+    img.src = '../assets/imgs/coffe.png';
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,9 +16,12 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     FormsModule,
+    MatInputModule,
     BrowserModule,
+    MatButtonModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
